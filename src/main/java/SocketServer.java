16b45fab12e2f3a18dd10cd3860048fead193017
Kol Crooks <kolcrooks10@gmail.com>
2019-02-10 21:42:57 -0500

    init commit

---
+++ src/main/java/SocketServer.java
@@ -0,0 +1,48 @@
+import org.opencv.core.RotatedRect;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+public class SocketServer {
+    DatagramSocket socket;
+    int port;
+    String ip;
+
+    public SocketServer(int port, String ip){
+        try {
+            socket = new DatagramSocket();
+        }catch(Exception e){System.err.println(e);}
+        this.port = port;
+        this.ip = ip;
+    }
+
+    public void sendData(ArrayList<RotatedRect> targets, int lidar){
+        String toSend = "{" +
+                        "\"Time\": " + System.currentTimeMillis() + "," +
+                        "\"Targets\": [";
+        for(RotatedRect rect : targets){
+            toSend += "{ \"Center\": { " +
+                            "\"x\": " + rect.center.x +
+                            ",\"y\": " + rect.center.y +
+                        "}, \"Size\": {" +
+                            "\"w\": " + rect.size.width +
+                            ",\"h\": " + rect.size.height +
+                        "}, \"Angle\": " + rect.angle +
+                        "},";
+        }
+
+        toSend = toSend.substring(0,toSend.charAt(toSend.length()-1) == ',' ? toSend.length()-1 : toSend.length()) + "]";
+        toSend += "\"Lidar\": " + lidar + "}";
+
+        byte[] buf = toSend.getBytes();
+        DatagramPacket packet = null;
+        try {
+            packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(ip), port);
+            socket.send(packet);
+        }catch(Exception e){
+            System.err.println(e);
+        }
+    }
+}
